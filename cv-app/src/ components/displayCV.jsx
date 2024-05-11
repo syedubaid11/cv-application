a882d8de096d274af0c9a92f9954b86f71f8f9ba
syedubaid <syedabuubaid@gmail.com>
2024-05-11 22:16:44 +0530

    commit

--- cv-app/src/ components/displayCV.jsx
+++ cv-app/src/ components/displayCV.jsx
@@ -1,19 +1,85 @@
-import Personalinfo from "./personalinfo";
-import Skillsinfo from "./skills";
+import {useState} from 'react'
 
-export default function DisplayCV(props){
+
+export default function DisplayCV(){
+    const [cvInfo,setcvInfo]=useState({
+        personalinfo:"",
+        skillsinfo:"",
+        educationalinfo:"",
+        workexinfo:""
+    })
+
+function handleChange(event){
+        const name=event.target.name
+        const value=event.target.value
+        setcvInfo({
+            ...cvInfo,
+            [name]:value
+        }
+        )
+
+    }
 
     return(
-        <div className="displaycv"> 
-            <h1>Personal Details:</h1>
-            <p>{props.name}</p>
-            <h1>Skills</h1>
-            <p>{props.skillInfo}</p>
+        <div className='all'>
+          <div className='input-container'>  
+                <div>
+                    <h3>Personal Details</h3>
+                    <input
+                    type="text"
+                    placeholder="Enter your Personal Details"
+                    name="personalinfo"
+                    value={cvInfo.personalinfo}
+                    onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <h3>Skills</h3>
+                    <input
+                    type="text"
+                    placeholder="Enter your Skills"
+                    name="skillsinfo"
+                    value={cvInfo.skillsinfo}
+                    onChange={handleChange}
+                   />
+                </div> 
+                <div>
+                    <h3>Educational Info</h3>
+                    <input
+                    type="text"
+                    placeholder="Enter your Skills"
+                    name="educationalinfo"
+                    value={cvInfo.educationalinfo}
+                    onChange={handleChange}
+                   />
+                </div> 
+                <div>
+                    <h3>Work Experience</h3>
+                    <input
+                    type="text"
+                    placeholder="Enter your Skills"
+                    name="workexinfo"
+                    value={cvInfo.workexinfo}
+                    onChange={handleChange}
+                   />
+                </div> 
+
+          </div>
+
+          <div className='cv-display-container'>
+            <div className='heading'>
+            <h1>{cvInfo.personalinfo}</h1>
+            </div>
+                <h3>{cvInfo.skillsinfo}</h3>
+                <p>{cvInfo.educationalinfo}</p>
+                <p>{cvInfo.workexinfo}</p>
+          </div>   
+
         </div>
+
+    
+        
     )
-
-
-
-
-
 }
+
+
--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -2,39 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Personalinfo from './ components/personalinfo.jsx'
-import Skillsinfo from './ components/skills.jsx'
-import Workex from './ components/workex.jsx'
-import Educationalinfo from './ components/educationalinfo.jsx'
-import DisplayCV from './ components/displayCV.jsx'
-
-
+import DisplayCV from './ components/displaycv'
 
 function App(){
 
 return(
 
 <div>  
+<DisplayCV/>
 
-  <div className='containerOfAll'>
-
-    
-      <div className='container' style={{marginLeft:'1rem'}}>   
-
-        <Personalinfo/>
-        <Skillsinfo/>
-        <Workex/>
-        <Educationalinfo/>
-        <DisplayCV/>
-
-        
-
-      <button className='clear'>Clear</button>
-      </div>
-
-      
-      
-  </div>
 </div>
  
 )
